516b751db4104ab9805583912eba7af7ba8fdbda
Shabanaazmeen <167525258+Shabanaazmeen@users.noreply.github.com>
2024-04-26 09:55:46 +0530

    basic

---
+++ hw.py
@@ -0,0 +1,21 @@
+=x=5
+print(type(x))
+a=int(input('enter the number a'))
+b=int(input('enter the number b'))
+x=a+b
+print(x)
+y=a-b
+print(y)
+z=a*b
+print(z)
+e=a/b
+print(e)
+if a>b:
+    print('a is greater than b')
+elif a<b:
+    print('bis less than a')
+else:
+    print('a , b are equal')
+    
+    
+    
